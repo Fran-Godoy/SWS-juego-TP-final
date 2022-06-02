ac718f5f0a5395bb0ab8607011a0c43815a08693
Fran-Godoy <101577372+Fran-Godoy@users.noreply.github.com>
2022-06-02 10:36:44 -0300

    avances base

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            transform.position = new Vector3(0, 2.5f, 0);
+        }
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            transform.position = new Vector3(-3.5f, 1, 0);
+        }
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            transform.position = new Vector3(3.5f, 1, 0);
+            if (Input.GetKeyDown(KeyCode.W))
+            {
+                transform.position = new Vector3(3.5f, 2.5f, 0);
+            }
+        }
+    }
+}
