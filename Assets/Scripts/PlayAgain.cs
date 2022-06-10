c89cd797ff9e6a33163ab2cff0b180f11aafd571
Fran-Godoy <101577372+Fran-Godoy@users.noreply.github.com>
2022-06-10 10:40:15 -0300

    Win MenuComplete

---
+++ Assets/Scripts/PlayAgain.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class PlayAgain : MonoBehaviour
+{
+    public void Back()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex -1);
+    }
+}
---
+++ Assets/Timemana.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Timemana : MonoBehaviour
+{
+    public Text txtTimeFloored;
+    float currentTime;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update() {
+        currentTime = Time.timeSinceLevelLoad;
+        txtTimeFloored.text = Mathf.Floor(currentTime).ToString();
+        if (currentTime > 12)
+        {
+            SceneManager.LoadScene("GameWin"); ;
+        }
+    }
+}
