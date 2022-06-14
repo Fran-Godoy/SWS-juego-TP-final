3f11bd1c1135cce0dcfcdc38e978355e98ce2b6e
Fran-Godoy <101577372+Fran-Godoy@users.noreply.github.com>
2022-06-14 11:13:12 -0300

    Instantiator attempt

---
+++ Assets/Scripts/Instantiator.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Instantiator : MonoBehaviour
+{
+    public GameObject objectToClone;
+    int cloneAmount;
+    public void CloneObject()
+    {
+        cloneAmount = 5;
+        int i = 0;
+        while (i < cloneAmount)
+        {
+            Instantiate(objectToClone);
+            i++;
+        }
+
+
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
